70f66cb9a4f30573a014a08f0a6a63ee6c19c17c
Ulugbek Nagmatov <andynestrov@yandex.ru>
2023-01-09 00:43:16 +0500

    bugfix

--- src/components/MeaningList.js
+++ src/components/MeaningList.js
@@ -4,7 +4,7 @@ const MeanigList = ({ mean }) => {
     <div>
       {mean.map((val) =>
         val.meanings.map((means) =>
-          means.partOfSpeech == "noun" ? (
+          means.partOfSpeech === "noun" ? (
             <div>
               <h5 className="text-2xl font-bold mt-4">{means.partOfSpeech}:</h5>
               {means.definitions.map((def) => (
